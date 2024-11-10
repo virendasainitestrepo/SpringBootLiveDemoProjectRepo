63d4b2a948eaf6ab72544c22ceb2ea026c9b36e1
itengineersaini@gmail.com <itengineersaini@gmail.com>
2024-11-10 14:09:12 +0530

    Commit Code-for sonarTesting

--- src/main/java/com/virenautomationtesting/springboot/HelloWorldController.java
+++ src/main/java/com/virenautomationtesting/springboot/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.javabycode.springboot;
+package com.virenautomationtesting.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/virenautomationtesting/springboot/MyWebApplication.java
+++ src/main/java/com/virenautomationtesting/springboot/MyWebApplication.java
@@ -1,4 +1,4 @@
-package com.javabycode.springboot;
+package com.virenautomationtesting.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
