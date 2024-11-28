941caf864a1f19c77b376ffe842252d5de341fb5
itengineersaini@gmail.com <itengineersaini@gmail.com>
2024-11-28 16:07:09 +0530

    Commit Code-for sonarTesting

--- src/main/test/testcases.java
+++ src/main/test/testcases.java
@@ -1,10 +1,12 @@
 package test;
-import org.junit.*;
+import org.junit.Test;
 
 public class testcases {
 
     @Test
     public void add(){
+        HelloWorldController hc= new HelloWorldController();
+        hc.hello();
         System.out.println("print code");
     }
 
